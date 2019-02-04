70183ee9a4e0b1040c149d582d9725f47fb6ee49
Nick Gauthier <ngauthier@gmail.com>
2019-02-03 21:31:41 -0500

    ssr

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import { h, render, Component } from 'preact';
 
 export default class App extends Component {
-    render() {
-        return <p>hello2</p>;
+    timer: any
+
+    constructor() {
+        super();
+        // set initial time:
+        this.state = {
+            time: Date.now()
+        }
+    }
+
+    componentDidMount() {
+        // update time every second
+        this.timer = setInterval(() => {
+            this.setState({ time: Date.now() });
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        // stop when not renderable
+        clearInterval(this.timer);
+    }
+
+    render(props, state) {
+        let time = new Date(state.time).toLocaleTimeString();
+        return <span>{time}</span>;
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import App from "./App";
 import { h, render, Component } from "preact";
 
-render(<App />, document.querySelector('[data-react-component="App"]'));
+const root = document.getElementById('root')
+render(<App />, root, root.firstElementChild);
